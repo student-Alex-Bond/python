06b8da9c220e7a9cbb006854e68178bcfe99eae2
Alex <bondarenko.aleksandr.25@gmail.com>
2020-12-22 14:41:33 +0600

    dz

---
+++ dz/py_project/main.py
@@ -0,0 +1,11 @@
+age = 30
+name ='Александр'
+surname='Bond'
+print(surname,name,age,"годиков")
+city = str(input('Enter your city: '))
+timezone = str(input('Enter timezone: '))
+street = str(input('Как называется главная улица в товоем городе))): '))
+print(city)
+print(timezone)
+print(street)
+print('Мой город '+city+' у меня часовой пояс '+timezone+' и главная улица в городе это '+street+')))')
