b344f73ace338ce1f359f0b6857b6a1286f12156
Alex <bondarenko.aleksandr.25@gmail.com>
2021-01-02 10:03:26 +0600

    delete

--- задача2.py
+++
@@ -1,9 +0,0 @@
-def Data(var_1, var_2, var_3, var_4, var_5, var_6):
-    print(f'{var_1}, {var_2}, {var_3}, {var_4}, {var_5}, {var_6}, ')
-name = input("Ваше имя: ")
-surname = input("Ваша фамилия: ")
-year_of_birth = input("Ваш год рождения: ")
-e_mail = input("Ваша электронная почта: ")
-phone = input("Номер вашего телефона: ")
-city = input("Город вашего проживания: ")
-Data(var_1=name, var_2=surname, var_3=year_of_birth, var_4=e_mail, var_5=phone, var_6=city)
--- задача6.py
+++
@@ -1,6 +0,0 @@
-def int_func(arg):
-    return arg.title()
-
-
-arg = input('Введите строку из латинских букв: ')
-print(int_func(arg))
